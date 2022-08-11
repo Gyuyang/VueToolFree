9bb062b088b1529c271c87f9a42ecca00e031864
XQUANTAD\yuyang.gong <i928625516@sina.com>
2022-08-11 18:02:02 +0800

    123

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
+// const { resolve } = require('core-js/library/fn/promise')
+const path = require('path')
+function resolve (dir) {
+  return path.join(__dirname, dir)
+}
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // base:'./',
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+  },
+  devServer: {
+    open: true
+  }
 })
